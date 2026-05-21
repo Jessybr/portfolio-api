3bf814dd72e5784efac121410f369b0ac338c6e5
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-20 23:27:42 -0300

    feat: create perfilService and add getPerfil function

---
+++ services/perfilService.js
@@ -0,0 +1,17 @@
+import prisma from '../lib/prisma.js'
+
+async function getPerfil() {
+    const perfil = await prisma.perfil.findUnique({
+        where: {
+            id: 1
+        }
+    })
+
+    if(!perfil) {
+        throw new Error('Perfil não encontrado')
+    }
+
+    return { perfil }
+}
+
+export default { getPerfil }
