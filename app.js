5b72ee61e2b7a79833abf14aabcf95917501fa0d
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-21 20:51:43 -0300

    feat: add projectRoutes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import authRoutes from './routes/auth.routes.js'
 import perfilRoutes from './routes/perfil.routes.js'
+import projectRoutes from './routes/project.routes.js'
 
 const app = express()
 app.use(express.json())
 app.use(authRoutes)
 app.use(perfilRoutes)
+app.use(projectRoutes)
 
 export default app
