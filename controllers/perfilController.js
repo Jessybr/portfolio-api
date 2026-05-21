da3634c37a0b65521ef7b3bcf3da404962aed5fc
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-20 23:28:19 -0300

    feat: create perfilController and add getPerfil function

---
+++ controllers/perfilController.js
@@ -0,0 +1,13 @@
+import perfilService from '../services/perfilService.js'
+
+async function getPerfil(req, res) {
+    try{
+        const result = await perfilService.getPerfil()
+
+        return res.json(result)
+    } catch {
+        return res.status(401).json({error: error.message})
+    }
+}
+
+export default { getPerfil }
