d593cb9a2b7cfb79a56650892b699327190e801c
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-20 23:54:56 -0300

    feat: add route to update perfil

--- routes/perfil.routes.js
+++ routes/perfil.routes.js
@@ -5,5 +5,6 @@ import { authMiddleware } from '../middlewares/authMiddlewares.js'
 const router = express.Router()
 
 router.get('/perfil', authMiddleware, perfilController.getPerfil)
+router.patch('/perfil', authMiddleware, perfilController.updatePerfil)
 
 export default router
