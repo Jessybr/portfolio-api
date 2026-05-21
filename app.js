e2a4058fe68f8d14ae832ec0773da2dd0cf08df3
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-20 23:34:06 -0300

    feat: add perfilRoutes to app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import authRoutes from './routes/auth.routes.js'
+import perfilRoutes from './routes/perfil.routes.js'
 
 const app = express()
 app.use(express.json())
 app.use(authRoutes)
+app.use(perfilRoutes)
 
 export default app
