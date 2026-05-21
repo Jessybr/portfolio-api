3070d641b6840fc443915943d9cd0c883669d8e5
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-20 23:54:35 -0300

    feat: add updatePerfil function to perfilService

--- services/perfilService.js
+++ services/perfilService.js
@@ -12,4 +12,21 @@ async function getPerfil() {
     return { perfil }
 }
 
-export default { getPerfil }
+async function updatePerfil(data) {
+    const perfil = await prisma.perfil.findUnique({
+        where: { id: 1 }
+    })
+
+    if(!perfil) {
+        throw new Error('Perfil não encontrado')
+    }
+
+    const perfilUpdated = await prisma.perfil.update({
+        where: { id: 1 },
+        data
+    })
+
+    return { perfil }
+}
+
+export default { getPerfil, updatePerfil }
