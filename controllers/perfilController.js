31421fc2b5f9e16acde5352639751100d64093b1
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-20 23:53:42 -0300

    feat: add updatePerfil function to perfilController

--- controllers/perfilController.js
+++ controllers/perfilController.js
@@ -10,4 +10,15 @@ async function getPerfil(req, res) {
     }
 }
 
-export default { getPerfil }
+async function updatePerfil(req, res) {
+    try {
+        const data = req.body
+        const result = await perfilService.updatePerfil(data)
+
+        return res.json(result)
+    } catch {
+        return res.status(401).json({error: error.message})
+    }
+}
+
+export default { getPerfil, updatePerfil }
