0d67e55c200042409dee7228006f869aa67f8e07
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-26 15:21:11 -0300

    feat: create http error and response

---
+++ utils/response/httpResponse.js
@@ -0,0 +1,35 @@
+function created(res, message = null, data = null) {
+    return res.status(201).json({
+        status: 201,
+        success: true,
+        message,
+        data
+    })
+}
+
+function noContent(res, message = null) {
+    return res.status(204).json({
+        status: 204,
+        success: true,
+        message
+    })
+}
+
+function ok(res, message = null, data = null) {
+    return res.status(200).json({
+        status: 200,
+        success: true,
+        message,
+        data
+    })
+}
+
+function externalError(res, message = null) {
+    return res.status(500).json({
+        status: 500,
+        success: false,
+        message
+    })
+}
+
+export default { created, noContent, ok, externalError }
