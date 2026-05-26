15b081901d26b539d521b6b82e4035ac130955ee
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-26 16:56:46 -0300

    feat: create function to validate params in requesitions

---
+++ utils/paramsTechnologyHelper.js
@@ -0,0 +1,23 @@
+import { HttpError } from './error/HttpError.js'
+
+function checkBodyNeededToCreateTechnology(body) {
+    const paramsNeeded = ['nome', 'iconeSrc']
+
+    for (const param of paramsNeeded) {
+        if (!body[param]) {
+            throw new HttpError("O nome e o ícone são obrigatórios", 422)
+        }
+    }
+}
+
+function checkBodyInexistent(body) {
+    const paramsAll = ['nome', 'iconeSrc']
+
+    for (const param in body) {
+        if (!paramsAll.includes(param)) {
+            throw new HttpError(`O parâmetro ${param} não é permitido`, 422)
+        }
+    }
+}
+
+export default { checkBodyNeededToCreateTechnology, checkBodyInexistent }
