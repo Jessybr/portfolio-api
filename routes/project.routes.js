e5c984618e42e756c96e64cf5dc3cdd4b18346dc
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-26 17:24:20 -0300

    feat: add route to get all projects active

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -4,10 +4,11 @@ import { authMiddleware } from '../middlewares/authMiddlewares.js'
 
 const router = express.Router()
 
+router.get('/project', authMiddleware, projectController.getProjects)
 router.post('/project', authMiddleware, projectController.createProject)
+router.get('/project/active', authMiddleware, projectController.getActiveProjects)
 router.patch('/project/:id', authMiddleware, projectController.updateProjectById)
 router.get('/project/:id', authMiddleware, projectController.getProjectById)
-router.get('/project', authMiddleware, projectController.getProjects)
 router.delete('/project/:id', authMiddleware, projectController.deleteProjectById)
 router.patch('/project/active/:id', authMiddleware, projectController.toggleActiveProject)
 
