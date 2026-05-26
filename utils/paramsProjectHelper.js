84699d4203021e00ba8cccbe52bc4b0aff3d0d52
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-26 15:20:56 -0300

    feat: create function to verify params

---
+++ utils/paramsProjectHelper.js
@@ -0,0 +1,23 @@
+import { HttpError } from './error/HttpError.js'
+
+function checkParamsNeeded(body) {
+    const paramsNeeded = ['nome', 'descricao']
+
+    for(const param in paramsNeeded) {
+        if(!body[param]) {
+            throw new HttpError("O nome e a descrição são obrigatórios", 422)
+        }
+    }
+}
+
+function checkParamsInexist(body) {
+    const paramsAll = ['nome', 'descricao', 'videoSrc', 'imagemSrc', 'deployUrl', 'githubUrl']
+
+    for(const param in body) {
+        if(!paramsAll.includes(param)) {
+            throw new HttpError(`O parâmetro ${param} não é permitido`, 422)
+        }
+    }
+}
+
+export default { checkParamsNeeded, checkParamsInexist }
