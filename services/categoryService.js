90c8f0223f0918b7019744fc2b6c994bb69e80fc
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-28 21:25:52 -0300

    feat: create function to create category

--- services/categoryService.js
+++ services/categoryService.js
@@ -19,3 +19,24 @@ async function getCategoryById(id) {
     return { category }
 }
 
+async function createCategory(data) {
+    const { nome } = data
+
+    const categoryAlreadyExists = await prisma.category.findFirst({
+        where: { nome }
+    })
+
+    if(categoryAlreadyExists) {
+        throw new HttpError(`Categoria com o nome: ${nome} já existe`, 409)
+    }
+
+    const newCategory = await prisma.category.create({
+        data: {
+            nome
+        }
+    })
+
+    return { newCategory }
+}
+
+export default { getCategories, getCategoryById, createCategory }
