1760531fb5e0de34d3d461c0fce988c51b488436
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-28 21:27:21 -0300

    feat: create function getCategoryById

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -11,3 +11,13 @@ async function getCategories(req, res) {
     }
 }
 
+async function getCategoryById(req, res) {
+    try {
+        const id = req.params.id
+        const category = await categoryService.getCategoryById(parseInt(id))
+
+        return responseHTTP.ok(res, null, category)
+    } catch (error) {
+        return responseHTTP.externalError(res, error.message, error.statusCode)
+    }
+}
