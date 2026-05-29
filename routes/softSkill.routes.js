f246ea3d27e32ce550d4187bc3296a9c2c4855df
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-29 13:52:52 -0300

    feat: add route to delete a softskill

--- routes/softSkill.routes.js
+++ routes/softSkill.routes.js
@@ -7,5 +7,6 @@ const router = express.Router()
 router.post('/softSkill', authMiddleware, softSkillController.createSoftSkill)
 router.get('/softSkills', authMiddleware, softSkillController.getSoftSkills)
 router.get('/softSkill/:id', authMiddleware, softSkillController.getSoftSkillById)
+router.delete('/softSkill/:id', authMiddleware, softSkillController.deleteSoftSkill)
 
 export default router
