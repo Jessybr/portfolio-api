636da0e1611c766b6a78be34be72a58a9afaf68c
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-29 12:35:26 -0300

    feat: create createSoftSkill function

--- controllers/softSkillController.js
+++ controllers/softSkillController.js
@@ -1,3 +1,16 @@
 import softSkillService from '../services/softSkillService.js'
 import responseHTTP from '../utils/response/httpResponse.js'
 
+async function createSoftSkill(req, res) {
+    try {
+        const data = req.body
+        const result = await softSkillService.createSoftSkill(data)
+
+        return responseHTTP.created(res, "Soft skill criada com sucesso!", result)
+    } catch (error) {
+        console.error(error) //retirar depois
+        return responseHTTP.externalError(res, error.message, error.statusCode)
+    }
+}
+
+export default { createSoftSkill }
--- services/softSkillService.js
+++ services/softSkillService.js
@@ -2,5 +2,27 @@ import prisma from "../lib/prisma.js"
 import checkersParams from "../utils/paramsSoftSkillHelper.js"
 import { HttpError } from "../utils/error/HttpError.js"
 
+async function createSoftSkill(data) {
+    checkersParams.checkBodyNeededToCreateSoftSkill(data)
+    checkersParams.checkBodyInexistent(data)
 
+    const { nome, iconeSrc } = data
 
+    const softSkillAlreadyExists = await prisma.softSkill.findFirst({
+        where: {
+            nome: nome
+        }
+    })
+
+    if(softSkillAlreadyExists) {
+        throw new HttpError("Soft skill already exists", 409)
+    }
+
+    const softSkill = await prisma.softSkill.create({
+        data
+    })
+
+    return { softSkill }
+}
+
+export default { createSoftSkill }
