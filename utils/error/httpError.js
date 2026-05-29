32c303d032f6131c091656a0fb865ebda4eec0f1
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-29 12:41:06 -0300

    refactor: add generic message

--- utils/error/httpError.js
+++ utils/error/httpError.js
@@ -1,5 +1,5 @@
 export class HttpError extends Error {
-    constructor(message, statusCode = 500) {
+    constructor(message = "Erro interno no servidor", statusCode = 500) {
         super(message)
         this.statusCode = statusCode
     }
