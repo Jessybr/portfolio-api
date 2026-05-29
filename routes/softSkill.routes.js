58cb70773df2ca813739516894fcfa15ef5128ba
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-29 13:46:02 -0300

    feat: add route to get softskill by id

--- routes/softSkill.routes.js
+++ routes/softSkill.routes.js
@@ -6,5 +6,6 @@ const router = express.Router()
 
 router.post('/softSkill', authMiddleware, softSkillController.createSoftSkill)
 router.get('/softSkills', authMiddleware, softSkillController.getSoftSkills)
+router.get('/softSkill/:id', authMiddleware, softSkillController.getSoftSkillById)
 
 export default router
