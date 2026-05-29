31ac6c1ff60918fb1cf87294fbf564e21a23d226
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-29 14:00:29 -0300

    refactor: reorganize routes

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -5,11 +5,11 @@ import { authMiddleware } from '../middlewares/authMiddlewares.js'
 const router = express.Router()
 
 router.get('/project', authMiddleware, projectController.getProjects)
-router.post('/project', authMiddleware, projectController.createProject)
 router.get('/project/active', authMiddleware, projectController.getActiveProjects)
-router.patch('/project/:id', authMiddleware, projectController.updateProjectById)
+router.post('/project', authMiddleware, projectController.createProject)
 router.get('/project/:id', authMiddleware, projectController.getProjectById)
-router.delete('/project/:id', authMiddleware, projectController.deleteProjectById)
+router.patch('/project/:id', authMiddleware, projectController.updateProjectById)
 router.patch('/project/active/:id', authMiddleware, projectController.toggleActiveProject)
+router.delete('/project/:id', authMiddleware, projectController.deleteProjectById)
 
 export default router
