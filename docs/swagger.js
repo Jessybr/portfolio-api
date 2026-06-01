92101b262f4faae3ed2506196b1ccf9fb9d85113
Jessybr <jessicabuenoramos5@gmail.com>
2026-06-01 18:15:24 -0300

    feat: create swagger.js to configure documentation

---
+++ docs/swagger.js
@@ -0,0 +1,37 @@
+import swaggerJsdoc from 'swagger-jsdoc'
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Portfolio API',
+            version: '1.0.0',
+            description: 'API REST para gerenciamento de portfolio'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: 'Servidor local'
+            }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT'
+                }
+            }
+        },
+        security: [
+            {
+                bearerAuth: []
+            }
+        ]
+    },
+    apis: ['./routes/*.js']
+}
+
+const swaggerSpec = swaggerJsdoc(options)
+
+export default swaggerSpec
