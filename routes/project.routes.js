dc7dbf74e72e7b50812ba955ce3838ea0956e186
Jessybr <jessicabuenoramos5@gmail.com>
2026-06-01 17:19:12 -0300

    feat: add routes to add and remove category and technology

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -11,5 +11,9 @@ router.get('/project/:id', authMiddleware, projectController.getProjectById)
 router.patch('/project/:id', authMiddleware, projectController.updateProjectById)
 router.patch('/project/active/:id', authMiddleware, projectController.toggleActiveProject)
 router.delete('/project/:id', authMiddleware, projectController.deleteProjectById)
+router.patch('/project/:id/technology', authMiddleware, projectController.addTechnologyToProject)
+router.delete('/project/:id/technology', authMiddleware, projectController.removeTechnologyFromProject)
+router.patch('/project/:id/category', authMiddleware, projectController.addCategoryToProject)
+router.delete('/project/:id/category', authMiddleware, projectController.removeCategoryFromProject)
 
 export default router
