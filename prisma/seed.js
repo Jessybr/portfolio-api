4df54563541cd8cf02d7e001834a78efd5a00b33
Jessybr <jessicabuenoramos5@gmail.com>
2026-06-02 21:07:52 -0300

    refactor: move database

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,7 +4,7 @@ import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 import bcrypt from 'bcrypt'
 
 const adapter = new PrismaBetterSqlite3({
-   url: process.env.DATABASE_URL ?? 'file:./dev.db'
+   url: process.env.DATABASE_URL ?? 'file:./database/dev.db'
 })
 
 const prisma = new PrismaClient({ adapter })
