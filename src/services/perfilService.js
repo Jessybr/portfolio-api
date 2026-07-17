323452badb2fe6f7ba8619493cd1da6a40c69fa1
Jessybr <jessicabuenoramos5@gmail.com>
2026-07-17 17:22:55 -0300

    feat: add uploadFile and deleteFile function to updatePerfil

--- src/services/perfilService.js
+++ src/services/perfilService.js
@@ -1,4 +1,5 @@
 import prisma from '../lib/prisma.js'
+import uploadService from './uploadService.js'
 
 async function getPerfil() {
     const perfil = await prisma.perfil.findUnique({
@@ -21,9 +22,24 @@ async function updatePerfil(data) {
         throw new Error('Perfil não encontrado')
     }
 
+    if(data.imagem) {
+        const image = await uploadService.uploadFile(data.imagem)
+        await uploadService.deleteFile(perfil.imagemPublicId, 'image')
+    }
+    if(data.pdf) {
+        const pdf = await uploadService.uploadFile(data.pdf)
+        await uploadService.deleteFile(perfil.pdfPublicId, 'raw')
+    }
+
     const perfilUpdated = await prisma.perfil.update({
         where: { id: 1 },
-        data
+        data: {
+            ...data,
+            fotoSrc: data.imagem ? image.url : perfil.fotoSrc,
+            imagemPublicId: data.imagem ? image.publicId : perfil.imagemPublicId,
+            curriculoSrc: data.pdf ? pdf.url : perfil.curriculoSrc,
+            curriculoPublicId: data.pdf ? pdf.publicId : perfil.curriculoPublicId
+        }
     })
 
     return { perfil }
