a5da629a010824b8501452378ff26791e8b78614
Jessybr <jessicabuenoramos5@gmail.com>
2026-07-17 17:16:33 -0300

    feat: add uploadRoutes

--- src/app.js
+++ src/app.js
@@ -5,11 +5,14 @@ import projectRoutes from './routes/project.routes.js'
 import technologyRoutes from './routes/technology.routes.js'
 import categoryRoutes from './routes/category.routes.js'
 import softSkillRoutes from './routes/softSkill.routes.js'
+import uploadRoutes from './routes/upload.routes.js'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec from '../docs/swagger.js'
+import cors from "cors";
 
 
 const app = express()
+app.use(cors());
 app.use(express.json())
 app.use(authRoutes)
 app.use(perfilRoutes)
@@ -17,6 +20,7 @@ app.use(projectRoutes)
 app.use(technologyRoutes)
 app.use(categoryRoutes)
 app.use(softSkillRoutes)
+app.use(uploadRoutes)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 export default app
