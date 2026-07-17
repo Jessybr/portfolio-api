9b74aae5c1aa15f519397dd2625245a6ad5b5948
Jessybr <jessicabuenoramos5@gmail.com>
2026-07-17 17:11:38 -0300

    feat: add columns to cloudinary files public id

---
+++ prisma/migrations/20260715201622_add_cloudinary_public_ids/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Perfil" ADD COLUMN "curriculoPublicId" TEXT;
+ALTER TABLE "Perfil" ADD COLUMN "fotoPublicId" TEXT;
+
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN "imagemPublicId" TEXT;
+ALTER TABLE "Project" ADD COLUMN "videoPublicId" TEXT;
