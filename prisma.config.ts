d8c166462c91a4711a81ef13be6513ebed62616f
Jessybr <jessicabuenoramos5@gmail.com>
2026-09-14 17:29:40 -0300

    feat: add variable verification

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,10 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL não foi informada nas variáveis de ambiente.");
+}
+
 export default defineConfig({
     schema: "prisma/schema.prisma",
 
